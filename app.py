944691e5ed9e61f60bd6a6be8fafd07b0f3e97ce
Krizzna69 <jaswanthuchiha69@gmail.com>
2025-02-17 09:51:30 +0530

    changed render url

--- app.py
+++ app.py
@@ -67,9 +67,6 @@ def home():
 @app.route('/second')
 def second():
     return render_template('second.html')
-@app.route('/third')
-def third():
-    return render_template('room2.html')
 @app.route("/index", methods=["GET", "POST"])
 def upload_image():
     """Handle image upload and similarity search."""
